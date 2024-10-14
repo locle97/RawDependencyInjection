31ca9e905fee53981484795dd60c0be40b141657
Loc Le <lpl212757@gmail.com>
2024-10-15 03:44:11 +0700

    feat - first init project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Dumpify;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        typeof(ServiceA).GetConstructors()
+            .First()
+            .GetParameters()
+            .Select(param =>
+            {
+                return new { param.Name, param.ParameterType };
+            })
+            .Dump();
+    }
+}
+
+public class ServiceA
+{
+    public ServiceA(string a)
+    {
+    }
+}

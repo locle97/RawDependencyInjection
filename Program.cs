5ce71ce8ebf82ab71dca204c0db38a180ce6f533
Loc Le <lpl212757@gmail.com>
2024-10-15 21:49:47 +0700

    feat - basic structure first

--- Program.cs
+++ Program.cs
@@ -1,24 +1,52 @@
-﻿using System.Reflection;
-using Dumpify;
+﻿using Dumpify;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        typeof(ServiceA).GetConstructors()
-            .First()
-            .GetParameters()
-            .Select(param =>
-            {
-                return new { param.Name, param.ParameterType };
-            })
-            .Dump();
+        Repository repo = new Repository();
+        Service service = new Service(repo);
+        Controller controller = new Controller(service);
+
+        controller.Render();
     }
 }
 
-public class ServiceA
+public class Controller
 {
-    public ServiceA(string a)
+    private readonly Service _service;
+
+    public Controller(Service service)
     {
+        _service = service;
+    }
+
+    public void Render()
+    {
+        string name = _service.GetName();
+        $"Hello {name}!".Dump();
+    }
+}
+
+public class Service
+{
+    private readonly Repository _repository;
+
+    public Service(Repository repository)
+    {
+        _repository = repository;
+    }
+
+    public string GetName()
+    {
+        return _repository.FetchName();
+    }
+}
+
+public class Repository
+{
+    public string FetchName()
+    {
+        return "Loc";
     }
 }
